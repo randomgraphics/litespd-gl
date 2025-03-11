4b62f9ce34f2f1e6ba48d6b73d0d33dfcb809444
Chen Li <chen.li@innopeaktech.com>
2025-03-11 12:03:27 -0700

    wip

---
+++ dev/sample/warp-grid.cpp
@@ -0,0 +1,43 @@
+#include "../lgl.h"
+
+struct Scene {
+
+    Scene() = default;
+
+    ~Scene() { quit(); }
+
+    bool init() {
+        // create a 4x4 mesh
+        return true;
+    }
+    
+    void quit() {
+        //
+    }
+    
+    void render() {
+        //
+    }        
+};
+
+int main() {
+    // Create an OpenGL context with all default options.
+    litespd::gl::RenderContext rc({});
+
+    Scene s;
+
+    if (!s.init()) return -1;
+
+    // Main loop
+    while (rc.beginFrame()) {
+        // Clear the screen
+        glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // render the frame
+        s.render();
+
+        // Swap the buffers
+        rc.endFrame();
+    }
+}
